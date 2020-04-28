d66246fb03b609cedbf8dfdc54c3850e45a69a17
Oliver O'Halloran <oohall@gmail.com>
2020-04-28 20:25:59 +1000

    add some selftests

---
+++ selftests/test_console.py
@@ -0,0 +1,57 @@
+import time
+
+import pytest
+
+import optest.OpTestConsole as con
+
+# FIXME: this is all pretty crappy, better than nothing i guess
+
+# NB: We need to deal with "real" SSH shell sessions and SSH sessions
+# to virtual serial ports. The latter will retain it's state provided
+# the shell isn't logged out and that drives the odd behaviour here.
+
+def test_nosetup_raises():
+    ssh = con.OpTestSSH("ozrom2-bmc", "root", "0penBmc")
+    with pytest.raises(RuntimeError):
+        ssh.run_command("ls -1")
+
+def test_sudo_raises():
+    ssh = con.OpTestSSH("ozrom2-bmc", "root", "0penBmc")
+    with pytest.raises(ValueError):
+        ssh.run_command("sudo ls -1")
+
+def test_ssh_shell():
+    ssh = con.OpTestSSH("ozrom2-bmc", "root", "0penBmc")
+
+    ssh.connect()
+    ssh.shell_setup()
+    ssh.run_command("ls -1")
+    ssh.close()
+
+    with pyexpect.raises():
+        ssh.run_command("ls -1")
+
+    #  In this case it's up to the caller to configure the shell
+    ssh.connect()
+    ssh.shell_setup()
+    ssh.close()
+
+# Hmm, how do we really test the setup behaviour?
+
+def test_ssh_vserial():
+    ssh = con.OpTestSSH("ozrom2-bmc", "root", "0penBmc", port=2200)
+
+    ssh.connect()
+    ssh.shell_setup()
+    ssh.run_command("ls -1")
+
+    # before closing grab the pexpect object directly and reset the shell
+    ssh.pty.sendline("exit")
+    time.sleep(1)
+    ssh.pty.sendline("x")
+    ssh.close()
+
+    # since it's a virtual serial we expect the prompt to stay setup
+    ssh.connect()
+    ssh.run_command("ls -1", timeout=2) #
+    ssh.close()
---
+++ selftests/test_imports.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3 
+
+import optest
+import optest.Exceptions
+import optest.OpExpect
+import optest.OpTestAMIWeb
+import optest.OpTestASM
+import optest.OpTestBMC
+import optest.OpTestConsole
+import optest.OpTestConstants
+import optest.OpTestCronus
+import optest.OpTestError
+import optest.OpTestFSP
+import optest.OpTestHMC
+import optest.OpTestHost
+import optest.OpTestInstallUtil
+import optest.OpTestIPMI
+import optest.OpTestKeys
+import optest.OpTestLogger
+import optest.OpTestMambo
+import optest.OpTestOpenBMC
+import optest.OpTestQemu
+import optest.OpTestSOL
+import optest.OpTestSSH
+import optest.OpTestSystem
+import optest.OpTestTConnection
+import optest.OpTestThread
+import optest.OpTestUtil
+import optest.SerialConsole
+
+def test_import():
+    pass
