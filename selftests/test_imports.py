aa02a21be687e0ba627dd017a3da81e367cccfd1
Oliver O'Halloran <oohall@gmail.com>
2020-05-01 13:10:36 +1000

    cleanup importest a bit

--- selftests/test_imports.py
+++ selftests/test_imports.py
@@ -2,36 +2,43 @@
 
 import optest
 
+# TODO: these are all broken at the momment,  move them into the active
+# list as they get ported over to the new system model
+
 #import optest.amiweb
-#import optest.asm
-#import optest.bmc
-#import optest.constants
+#import optest.bmc      # convert to new system, possibly split SMC and AMI BMCs, rename
+#import optest.openbmc  # convert to new system / console, etc
+#import optest.mambo    # convert to new system type
+
+#import optest.ipmi     # covert IPMIConsole
 #import optest.cronus
-#import optest.fsp
+
+#import optest.fsp # system conversion
+#import optest.asm
 #import optest.hmc
-#import optest.host
+
+#import optest.host # i need to figure out what this is supposed to do
+
 #import optest.installutil
-#import optest.ipmi
-#import optest.keys
-#import optest.mambo
-#import optest.openbmc
-#import optest.sol
-#import optest.telnet
+
 #import optest.thread
-#import optest.util die
+#import optest.sol      # we need to re-think how this works
+#import optest.util
 
-
-#import optest.ssh # just provides OpTestSSH, can probably become a CommandConsole
+#import optest.telnet # covert to new Console
 
 import optest.exceptions
 import optest.system
 import optest.qemu
 import optest.opexpect
-
+import optest.petitboot
 import optest.console
 
-import optest.logger
+import optest.constants
+import optest.keys     # should get folded into constants
 
+# FIXME: we really should rename this something better
+import optest.logger
 
 def test_import():
     pass
