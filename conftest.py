fbca6f11352a570df282708e390f646fe1fafb3d
Oliver O'Halloran <oohall@gmail.com>
2020-05-06 00:53:03 +1000

    add result checking example
    see:
    https://docs.pytest.org/en/2.7.3/example/simple.html#post-process-test-reports-failures
    
    we might be able to use this to "fix" the system by re-IPLing after a
    test failure.

--- conftest.py
+++ conftest.py
@@ -40,6 +40,30 @@ from optest import logger
 global optestlog
 optestlog = logging.getLogger(logger.optest_logger_glob.parent_logger)
 
+import pytest
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    # execute all other hooks to obtain the report object
+    outcome = yield
+    rep = outcome.get_result()
+
+    # set a report attribute for each phase of a call, which can
+    # be "setup", "call", "teardown"
+
+    setattr(item, "rep_" + rep.when, rep)
+
+@pytest.fixture
+def something(request):
+    yield
+    # request.node is an "item" because we use the default
+    # "function" scope
+    if request.node.rep_setup.failed:
+        print("setting up a test failed!", request.node.nodeid)
+    elif request.node.rep_setup.passed:
+        if request.node.rep_call.failed:
+            print("executing test failed", request.node.nodeid)
+
 '''
 # create loggers, take hostlocks, etc
 # FIXME: work out what to do with all this logging stuff. We might be able to
